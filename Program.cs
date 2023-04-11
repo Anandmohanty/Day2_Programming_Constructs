d7e6caa14a64ed55998add70eb692003a36e1c13
Anandmohanty <mohantyanand3@gmail.com>
2023-04-11 12:14:30 +0530

    Welcome to programmin contruct

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day2_Programming_Constructs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Programming construct");
+        }
+    }
+}
